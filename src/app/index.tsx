c9fe5aa0a2a7b71f9ffc3ddc023bff1eecd3f4e9
Masa11111 <m1s0h3d1@icloud.com>
2025-02-22 23:32:50 +0900

    create Header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,11 @@
 import { StyleSheet, Text, View } from "react-native"
+import Header from "../components/Header"
 
 const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
 
-            <View style={styles.header}>
-                <View style={styles.headerInner}>
-                    <Text style={styles.headerTitle}>Memo App</Text>
-                    <Text style={styles.headerRight}>ログアウト</Text>
-                </View>
-            </View>
+            <Header></Header>
 
             <View>
                 <View style={styles.memoListItem}>
@@ -57,27 +53,7 @@ const styles = StyleSheet.create({
         flex: 1, // 画面全体に要素を広げる
         backgroundColor: '#ffffff'
     },
-    header: {
-        backgroundColor: "#467FD3",
-        height: 104,
-        justifyContent: 'flex-end'
-    },
-    headerInner: {
-        alignItems: 'center',
-    },
-    headerTitle: {
-        marginBottom: 8,
-        fontSize: 22,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        color: '#ffffff'
-    },
-    headerRight: {
-        position: 'absolute',
-        right: 16,
-        bottom: 16,
-        color: 'rgba(255, 255, 255, 0.7)'
-    },
+
     memoListItem: {
         backgroundColor: '#ffffff',
         flexDirection: 'row',
